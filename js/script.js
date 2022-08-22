2a0da68f810407a4dd7fabf39a2d20cfc4cd06f7
Khalid-Hasan96 <khbappy66@gmail.com>
2022-08-23 02:56:49 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const playerNames = [];
 
 function displayPlayerName(displayPlayers) {
+    if (displayPlayers.length > 5) {
+        alert("You can't add more than 5 players");
+        return;
+    }
     const displaySinglePlayerName = document.getElementById('display-players');
     displaySinglePlayerName.innerHTML = '';
 
@@ -9,19 +13,64 @@ function displayPlayerName(displayPlayers) {
 
         const li = document.createElement('li');
         li.innerHTML = `
-        <li>${i + 1}. ${singelPlayerName}</li>
+        <li class="text-xl font-semibold">${i + 1}. ${singelPlayerName}</li>
         
         `;
         displaySinglePlayerName.appendChild(li);
-
     }
 
 }
+function disableData() {
+    const selectButtons = document.querySelectorAll('button');
+    selectButtons.style.backgroundColor = 'green';
+}
+
 function addPlayerOnClick(playerId) {
     const playerName = playerId.parentNode.parentNode.children[1].innerText;
     const playerObj = { singlePlayerName: playerName };
     playerNames.push(playerObj);
     displayPlayerName(playerNames);
-
 }
+
+
+
+function getInputFields(inputId) {
+    const getInputField = document.getElementById(inputId);
+    const getInputElementString = getInputField.value;
+    const getInputElement = parseFloat(getInputElementString);
+    getInputField.value = '';
+    return getInputElement;
+}
+
+function getAndSetElemnetValue(elementId, newValue) {
+    const getElements = document.getElementById(elementId);
+    const getElementsString = getElements.innerText;
+    const elements = parseFloat(getElementsString);
+    getElements.innerText = newValue;
+    return elements;
+}
+
+// calculating & displaying player cost
+document.getElementById('calc-player-cost').addEventListener('click', function () {
+    const getPlayerCostInput = getInputFields('per-player-total')
+    const totalPlayerCost = getPlayerCostInput * 5;
+    getAndSetElemnetValue('total-player-cost', totalPlayerCost);
+
+})
+
+
+document.getElementById('calculate-total').addEventListener('click', function () {
+    const totalPlayerCost = document.getElementById('total-player-cost');
+    const totalPlayerCostString = totalPlayerCost.innerText;
+    const playerCost = parseFloat(totalPlayerCostString);
+
+    const managerCost = getInputFields('manager-cost');
+    const coachCost = getInputFields('coach-cost');
+
+    const totalCost = playerCost + managerCost + coachCost;
+
+    getAndSetElemnetValue('total-cost', totalCost);
+})
+
+
 
